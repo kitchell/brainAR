c76180068a604d78f33573978b21b858366ed060
kitchell <lindseykitchell21@gmail.com>
2019-10-12 16:56:58 -0400

    update

--- buttonjs.js
+++ buttonjs.js
@@ -3,6 +3,6 @@ AFRAME.registerComponent('rotateLeft', {
    init: function(){
        var brainEl = document.querySelector('#brain');
        document.querySelector('#left').addEventListener('click', function() {
-           brainEL.object3D.rotation.set(-90,-90,0);
+           brainEl.object3D.rotation.set(-90,-90,0);
        })
 }});
