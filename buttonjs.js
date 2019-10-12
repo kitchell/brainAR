e85fac74d2130eec76bc27cdf623a3f01510cfa0
kitchell <lindseykitchell21@gmail.com>
2019-10-12 18:20:14 -0400

    update

--- buttonjs.js
+++ buttonjs.js
@@ -1,15 +1,6 @@
-var scene = document.querySelector('a-scene');
-
-if (scene.hasLoaded) {
-    init()
-} else{
-    scene.addEventListener('loaded', init);
-}
-
-
-function init() {
-        
-    var brainEl = document.querySelector('#brain');
+AFRAME.registerComponent('run', {
+    init: function () {
+         var brainEl = document.querySelector('#brain');
     console.log(brainEl)
     
     document.querySelector('#left').addEventListener('click', leftView);
@@ -21,4 +12,21 @@ function init() {
 
        console.log('left button clicked');
     }
-}
+    }
+})
+
+//function init() {
+//        
+//    var brainEl = document.querySelector('#brain');
+//    console.log(brainEl)
+//    
+//    document.querySelector('#left').addEventListener('click', leftView);
+//
+//    function leftView(event) {
+//
+//       brainEl.object3D.rotation.y = THREE.Math.degToRad(-90);
+//       brainEl.object3D.rotation.x = THREE.Math.degToRad(-90);
+//
+//       console.log('left button clicked');
+//    }
+//}
