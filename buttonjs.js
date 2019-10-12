7f3c2b56ebf11bae4432850de67470918e0782ea
kitchell <lindseykitchell21@gmail.com>
2019-10-12 17:51:15 -0400

    update

--- buttonjs.js
+++ buttonjs.js
@@ -1,12 +1,11 @@
+function init() {
+        
+document.querySelector('#left').addEventListener('click', function() {
+    var brainEl = document.querySelector('#brain');
+    console.log(brainEl)
+   brainEl.object3D.rotation.y = THREE.Math.degToRad(-90);
+   brainEl.object3D.rotation.x = THREE.Math.degToRad(-90);
 
-AFRAME.registerComponent('rotateLeft', {
-   init: function(){
-       var brainEl = document.querySelector('#brain');
-       console.log(brainEl);
-       document.querySelector('#left').addEventListener('click', function() {
-           brainEl.object3D.rotation.y = THREE.Math.degToRad(-90);
-           brainEl.object3D.rotation.x = THREE.Math.degToRad(-90);
-           
-           console.log('left button clicked');
-       })
-}});
+   console.log('left button clicked');
+})
+}
