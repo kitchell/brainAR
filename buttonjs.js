1dbc17e87a68905a6be82884717c8e65b33d13a2
kitchell <lindseykitchell21@gmail.com>
2019-10-12 20:02:17 -0400

    rotations set and buttons

--- buttonjs.js
+++ buttonjs.js
@@ -4,14 +4,51 @@ AFRAME.registerComponent('run', {
         console.log(brainEl)
     
         document.querySelector('#left').addEventListener('click', leftView);
-
+        document.querySelector('#front').addEventListener('click', frontView);
+        document.querySelector('#right').addEventListener('click', rightView);
+        document.querySelector('#back').addEventListener('click', backView);
+        document.querySelector('#top').addEventListener('click', topView);
+        document.querySelector('#bottom').addEventListener('click', bottomView);
+        
+        
         function leftView(event) {
-
-           brainEl.object3D.rotation.y = THREE.Math.degToRad(-90);
-           brainEl.object3D.rotation.z = THREE.Math.degToRad(-90);
-
+            brainEl.setAttribute('rotation', {x:360, y:270, z:90});
            console.log('left button clicked');
     }
+     
+        function frontView(event) {
+            brainEl.setAttribute('rotation', {x:-90, y:0, z:0});
+//            brainEl.object3D.rotation.x = THREE.Math.degToRad(-90);
+//           brainEl.object3D.rotation.y = 0;
+//           brainEl.object3D.rotation.z = 0;
+           console.log('front button clicked');
+    }
+        function rightView(event) {
+            brainEl.setAttribute('rotation', {x:180, y:270, z:90});
+           console.log('right button clicked');
+    }
+        function backView(event) {
+            brainEl.setAttribute('rotation', {x:90, y:180, z:0});
+//            brainEl.object3D.rotation.x = THREE.Math.degToRad(90);
+//           brainEl.object3D.rotation.y = 0;
+//           brainEl.object3D.rotation.z = 0;
+           console.log('back button clicked');
+    }
+        function topView(event) {
+             brainEl.setAttribute('rotation', {x:0, y:180, z:0});
+//            brainEl.object3D.rotation.x = 0;
+//           brainEl.object3D.rotation.y = 0;
+//           brainEl.object3D.rotation.z = 0;
+           console.log('top button clicked');
+    }
+        function bottomView(event) {
+            brainEl.setAttribute('rotation', {x:-180, y:0, z:0});
+//            brainEl.object3D.rotation.x = THREE.Math.degToRad(-180);
+//           brainEl.object3D.rotation.y = 0;
+//           brainEl.object3D.rotation.z = 0;
+           console.log('bottom button clicked');
+    }
+        
     }
 })
 
