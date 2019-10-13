8b8692964e660534dc99bd603feefcf70d11a580
kitchell <lindseykitchell21@gmail.com>
2019-10-12 21:21:07 -0400

    update positioning

--- buttonjs.js
+++ buttonjs.js
@@ -13,11 +13,13 @@ AFRAME.registerComponent('run', {
         
         function leftView(event) {
             brainEl.setAttribute('rotation', {x:360, y:270, z:90});
+            brainEl.setAttribute('position', {x: 0, y: 0, z: .7})
            console.log('left button clicked');
     }
      
         function frontView(event) {
             brainEl.setAttribute('rotation', {x:-90, y:0, z:0});
+            brainEl.setAttribute('position', {x: 0, y: 0, z: .7})
 //            brainEl.object3D.rotation.x = THREE.Math.degToRad(-90);
 //           brainEl.object3D.rotation.y = 0;
 //           brainEl.object3D.rotation.z = 0;
@@ -25,10 +27,12 @@ AFRAME.registerComponent('run', {
     }
         function rightView(event) {
             brainEl.setAttribute('rotation', {x:180, y:270, z:90});
+            brainEl.setAttribute('position', {x: 0, y: 0, z: .7})
            console.log('right button clicked');
     }
         function backView(event) {
             brainEl.setAttribute('rotation', {x:90, y:180, z:0});
+            brainEl.setAttribute('position', {x: 0, y: 0, z: .7})
 //            brainEl.object3D.rotation.x = THREE.Math.degToRad(90);
 //           brainEl.object3D.rotation.y = 0;
 //           brainEl.object3D.rotation.z = 0;
@@ -36,6 +40,7 @@ AFRAME.registerComponent('run', {
     }
         function topView(event) {
              brainEl.setAttribute('rotation', {x:0, y:180, z:0});
+            brainEl.setAttribute('position', {x: 0, y: -1.5, z: 0})
 //            brainEl.object3D.rotation.x = 0;
 //           brainEl.object3D.rotation.y = 0;
 //           brainEl.object3D.rotation.z = 0;
@@ -43,6 +48,7 @@ AFRAME.registerComponent('run', {
     }
         function bottomView(event) {
             brainEl.setAttribute('rotation', {x:-180, y:0, z:0});
+            brainEl.setAttribute('position', {x: 0, y: 1.2, z: 0})
 //            brainEl.object3D.rotation.x = THREE.Math.degToRad(-180);
 //           brainEl.object3D.rotation.y = 0;
 //           brainEl.object3D.rotation.z = 0;
